134d549cf682fcaab43397719a50ecb8dea4f149
zexiin <35815821+zexiin@users.noreply.github.com>
2018-06-15 12:18:10 -0400

    Add files via upload
    doesnt rly work rn but has string/array to rectangular block processing code

--- game.js
+++ game.js
@@ -1,83 +1,179 @@
-	// movements
 
-	const Game = function() {
+	var context = document.querySelector("canvas").getContext("2d");
+	var H = context.canvas.height = 1000;
+	var W = context.canvas.width = 800;
+	var SCALE = 30;
 
+			// COIN
+	function coin(x, y) {
+				    this.x = x;
+				    this.y = y;
 	}
 
-	Game.prototype {
-		constructor : Game
-	}
+coin.prototype.draw = function() {
 
+				    context.fillStyle = "#ffd175";
+				          context.beginPath();
+				          context.arc(this.x * SCALE, this.y * SCALE, SCALE/3, 0, 2*Math.PI);
+				          context.fill();
+
+				  };
+
+		    function wall(x,y) {
+		    	this.x = x;
+			    this.y = y;
+		    }
+
+		wall.prototype.draw = function() {
+
+				    context.fillStyle = "#aed891";
+		            context.fillRect(this.x * SCALE, this.y * SCALE, SCALE, SCALE);
+
+				  };
+
+	    function sprite() {
+	    this.rad = 8;
+	    this.jumping = true;
+
+	    this.x = 2;
+	    this.y = 2;
+
+	    this.x_vel = 0;
+	    this.y_vel = 0;
+	  };
  
-	Game.Sprite  = function() {
+	  sprite.prototype.stepX = function(dir) {
 
-		rad:8,
-	    jumping:true,
-	    x:160,
-	    y:0,
-	    x_vel:0,
-	    y_vel:0,
-
-	    jump:function() { 
-	    	 this.y_vel -= 20;
-             this.jumping = true;
-         },
-
-	    moveLeft:function() { this.x_vel -= 1; },
-	    moveRight:function() { this.x_vel += 1; },
-
-	    update:function() {
-	    	this.y_vel += 1; //gravity
-            this.x += sprite.x_vel;
-            this.y += sprite.y_vel;
-            this.x_vel *= 0.9;
-            this.y_vel *= 0.9;
+	    // left
+	    if (dir.type == "left") {
+	  	this.x_vel -= 1;
+	    } 
+	    else if (dir.type == "right") {
+	  	// right
+	  	this.x_vel += 1;
 	    }
 
-	};
+	  }
 
-	Game.Sprite.prototype = {
-		constructor : Movement.Sprite
+	  sprite.prototype.draw = function() {
+	  	context.fillStyle = "#ff0000";
+				          context.beginPath();
+				          context.arc(this.x * SCALE, this.y * SCALE, SCALE/3, 0, 2*Math.PI);
+				          context.fill();
 
-	};
+	  }
+	  
 
-	function collisionIDK(player, object) {
-	        let right = object.x + object.width;
-	        let left = object.x;
-	        let top = object.y;
-	        let bottom = object.y + object.height;
-	        let friction = 0.7;
+	  var control = {
+	    left:false,
+	    right:false,
+	    up:false,
 
-	        // if player is going left and hits right side of object
+	    keyListener:function(event) {
 
-	        if (player.x_vel < 0 && player.x <= right + player.rad && player.x >= right && player.y >= object.y && player.y <= object.y + object.height) {
-	            player.x_vel = -friction * player.x_vel;
-	            player.x = right + player.rad;
-	        }
+	      // switch the keystate
+	      var key_state = (event.type == "keydown")?true:false;
 
+	      switch(event.keyCode) {
 
+	        case 37: // left
+	          control.left = key_state;
+	          alert(left);
+	          break;
 
-	        // if player is going right and hits left side of object
+	        case 38: // up
+	          control.up = key_state;
+	          alert(up);
+	          break;
 
-	        if (player.x_vel > 0 && player.x >= left - player.rad && player.x <= left && player.y >= object.y && player.y <= object.y + object.height) {
-	            player.x_vel = -friction * player.x_vel;
-	            player.x = left - player.rad;
-	        }
+	        case 39: // right
+	          control.right = key_state;
+	          break;
 
-	        // if player is going down and hits top of object
-
-	        if (player.y_vel > 0 && player.x >= object.x && player.x <= object.x+ object.width && player.y >= top - player.rad && player.y <= bottom) {
-	            player.y_vel = -friction * player.y_vel;
-	            player.y = top - player.rad;
-	            player.jumping = false;
-	        }
-
-	        // if player is going up and hits bottom of object
-
-	        if (player.y_vel < 0 && player.x >= object.x && player.x <= object.x+ object.width && player.y <= bottom && player.y >= top) {
-	            player.y_vel = -friction * player.y_vel;
-	            player.y = bottom+player.rad;
-	        }
-
-
+	      }
 	    }
+	  };
+
+			       var simpleLevelPlan = [
+			  "                      ",
+			  "          o           ",
+			  "  x              = x  ",
+			  "  x      o  o o    x  ",
+			  "  x @      xxxxx   x  ",
+			  "  xxxxx            x  ",
+			  "      x!!!!!!!!!!!!x  ",
+			  "      xxxxxxxxxxxxxx  ",
+			  "                      "
+			];
+
+
+			function level(plan) {
+
+				this.height = plan.length;
+				this.width = plan[0].length;
+				// window.alert(this.height);
+				// window.alert(this.width);
+
+				// for each row
+				var i;
+				for (i = 0; i < this.height; i++) {
+					var row = plan[i]
+
+			    // for each column
+				    var j;
+					for (j = 0; j < this.width; j++) {
+
+						var pix = row[j];
+						 if (pix == 'o') {
+						 	var coin1 = new coin(j, i);
+						 	coin1.draw();
+						 }
+
+						 else if (pix == 'x') {
+						 	var wall1 = new wall(j, i);
+						 	wall1.draw();
+						 }
+
+				   }
+
+				}
+			}
+
+
+
+			var player = new sprite();
+
+
+			 loop = function() {
+
+			 	context.fillStyle = "#eaf7fc";
+     context.fillRect(0, 0, W, H);// x, y, width, height
+          
+
+			 	if (control.right) {
+			 		player.stepX(right);
+			 	}
+
+			 	else if (control.left) {
+			 		player.stepX(left);
+			 	}
+
+
+	  	player.y_vel += 1; //gravity
+	   player.x += player.x_vel;
+	   player.y += player.y_vel;
+
+			 player.draw();
+
+				level(simpleLevelPlan);
+
+			 	window.requestAnimationFrame(loop);
+
+	    
+			 }
+
+
+	  window.addEventListener("keydown", control.keyListener);
+	  window.addEventListener("keyup", control.keyListener);
+	  window.requestAnimationFrame(loop);
+			
