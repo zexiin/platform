ea8f507709ccf9ad1d661b81a7919157eafc9311
zexiin <35815821+zexiin@users.noreply.github.com>
2018-06-20 10:50:18 -0400

    Update game.js

--- camtest/game.js
+++ camtest/game.js
@@ -70,15 +70,15 @@ function init() {
 
 
 	map.init(38, 11, "\
- ]                                 [X \
- ]                                 [X \
- ]                                 [X \
- 8445     o                  %%    [X \
- XXX]               o       o %    [X \
- 9776   o    %     dmmb    o  o    [X \
- ]o        o     o        dmmmmb   [X \
+ ]                        o        [X \
+ ]                   o  dmmmb      [X \
+ ]  o  %%%     o  o                [X \
+ 8445    %%%%      dmmmmb          [X \
+ XXX]              ! o       o     [X \
+ 9776   o    %     dmmb    o !o    [X \
+ ]o        %  !  o        dmmmmb   [X \
  ]! oooo dmmmmb o o      o  o  o   [X \
- ]    o  ixxxxl   o    o         o [X \
+ ]    o  ixxxxl   o    o !    !  o [X \
  ========xxxxxx====================== \
  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ");
 
