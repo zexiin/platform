669c05a164840daaaa4e109c1dda2d40ec13dc41
zexiin <35815821+zexiin@users.noreply.github.com>
2018-06-22 14:29:13 -0400

    Update player.js

--- camtest/player.js
+++ camtest/player.js
@@ -9,26 +9,22 @@ the Player class.
 
 
 function Player() {
-	this.x = 100;
-	this.y = 0;
-	
-	
-	
-	this.xinit = 100;
-	this.yinit = 0;
+	this.x = this.xinit = 160;
+	this.y = this.yinit = 0;
 	this.x_vel = 0;
 	this.y_vel = 0;
 	this.jumping = false;
 
-	this.camCoords = { /*x:100, y:0 */}; //temp
+	this.camCoords = {}; //temp
 
-	this.GRAVITY = 0.3;
-	this.X_ACCEL = 0.5;
-	this.Y_ACCEL = 15;
+	this.GRAVITY = 0.15*scaleFactor; //0.15
+	this.X_ACCEL = 0.25*scaleFactor; // 0.25
+	this.Y_ACCEL = 8*scaleFactor;
 	this.FRICTION = 0.83;
 	this.w = 32*scaleFactor;
 	this.h = 32*scaleFactor;
-	
+
+
 	// define sprite bounding box
 	this.bound = {
 		x:0, 
@@ -40,21 +36,24 @@ function Player() {
 		x_offset: 11.5*scaleFactor, // 10
 		y_offset: 13*scaleFactor
 	};
+
+
 }
 
 
 Player.prototype.update = function() {
-	if (control.up && this.jumping === false) {
-      this.y_vel -= this.Y_ACCEL;
-      this.jumping = true;
-  	}
-  	
-    if (control.left) {
-      this.x_vel -= this.X_ACCEL;
-  	}
-    if (control.right) {
-      this.x_vel += this.X_ACCEL;
-  	}
+
+	// handle controls
+		if (control.up && !this.jumping) {
+	      this.y_vel -= this.Y_ACCEL;
+	      this.jumping = true;
+	  	}
+	    if (control.left) {
+	      this.x_vel -= this.X_ACCEL;
+	  	}
+	    if (control.right) {
+	      this.x_vel += this.X_ACCEL;
+	  	}
 
     this.y_vel += this.GRAVITY;
     this.x += this.x_vel;
@@ -70,7 +69,7 @@ Player.prototype.update = function() {
 	  this.y_vel *= this.FRICTION;
 	}	
 	// player's camera coordinates are updated in Camera.update()
-	
+
 	this.updateBoundingBox();
 
 };
@@ -83,6 +82,9 @@ Player.prototype.updateBoundingBox = function() {
 	this.bound.y = this.y + this.bound.y_offset;
 };
 
+
+
 Player.prototype.draw = function() {
+	//context.drawImage(sprite, 0, 0, 32, 32, this.x, this.y, this.w, this.h);
 	context.drawImage(sprite, 0, 0, 32, 32, this.camCoords.x, this.camCoords.y, this.w, this.h);
 };
