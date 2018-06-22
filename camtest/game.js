e82ad66d6c5ffecd847b92dd58edbe190c538c8a
zexiin <35815821+zexiin@users.noreply.github.com>
2018-06-22 11:41:31 -0400

    Update game.js

--- camtest/game.js
+++ camtest/game.js
@@ -7,6 +7,12 @@ this file contains the main game functions??
 
 var context = document.querySelector("canvas").getContext("2d");
 var canvas = context.canvas;
+var mapArr = [];
+
+context.font = "15px Arial";    
+context.textBaseline="top"; 
+context.fillText("Top",5,100);    
+
 canvas.height = 500;
 canvas.width = 480;
 
@@ -34,8 +40,6 @@ var control = {
 	}
 };
 
-
-
 /**** function calls in here ****/
 
 // don't start game loop until all images have been preloaded
@@ -43,33 +47,67 @@ var tilesheet = new Image();
 tilesheet.onload = function() { sprite.onload(); }; // on loading this, load next
 tilesheet.src = "../sheet.png";
 var sprite = new Image();
-sprite.onload = function() { init(); }; // on loading this, start initialize the game.
+
+// on loading this, start initialize the game.
+sprite.onload = function() { 
+init(map1); 
+}; 
+
 sprite.src = "../characters.png";
-
-
 
 var player, map, cam;
 
-function init() {
 
-	context.imageSmoothingEnabled = false;
-	player = new Player();
-	map = new Map(tilesheet, 16, 32);
-/*	map.init(23, 11, "\
+var map1 = {
+	map: "\
  ]                  [X \
  ]                  [X \
  ]                  [X \
  8445     o         [X \
- XXX]               [X \
+ XXX]     %         [X \
  9776   o    %      [X \
  ]o        o     o  [X \
  ]! oooo dmmmmb o o [X \
- ]    o  ixxxxl   o [X \
+ ]    o  ixxxxl t o [X \
  ========xxxxxx======= \
- xxxxxxxxxxxxxxxxxxxxx ");*/
+ xxxxxxxxxxxxxxxxxxxxx ",
+   row: 11, 
+   col: 23
 
+}
 
-	map.init(38, 21, "\
+ var map2 = {
+ 	name: "map2",
+    map: "\
+X]                        o        [XX\
+X]                                 [XX\
+X]            o    o               [XX\
+X]                     vt   o      [XX\
+X]        o          dmmmb         [XX\
+X]    o          o         o       [XX\
+X]       o   dmmmb   o     o   o   [XX\
+X]       2          o     o        [XX\
+X] o  dmmmb  o    o    o  o        [XX\
+X]            vvv     o   o      o [XX\
+X]    o   o  dmmmb        vv  o    [XX\
+X]                   o  dmmmb      [XX\
+X]v o        o  o  2         o     [XX\
+X8445    dmmmb      dmmmmb         [XX\
+XXXX]              ! o       o     [XX\
+X9776   o    %     dmmb o  o !o    [XX\
+X]o        %  !  o    o   dmmmmb   [XX\
+X]! oooo dmmmmb o o      o  o  o   [XX\
+X]2   o  ixxxxl   ovvvvo !    !  o [XX\
+=========xxxxxx=======================\
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  row: 21,
+  col: 38
+
+ }
+
+ var map3 = {
+ 	name: "map2",
+    map: "\
 X]                        o        [XX\
 X]                                 [XX\
 X]            o    o               [XX\
@@ -82,28 +120,42 @@ X] o  dmmmb  o    o    o  o        [XX\
 X]            vvv     o   o      o [XX\
 X]    o   o  dmmmb        vv  o    [XX\
 X]                   o  dmmmb      [XX\
-X]v o  %%%     o  o  2         o   [XX\
-X8445    %%%%      dmmmmb          [XX\
+X]v o        o  o  2         o     [XX\
+X8445    dmmmb      dmmmmb         [XX\
 XXXX]              ! o       o     [XX\
 X9776   o    %     dmmb o  o !o    [XX\
 X]o        %  !  o    o   dmmmmb   [XX\
 X]! oooo dmmmmb o o      o  o  o   [XX\
 X]2   o  ixxxxl   ovvvvo !    !  o [XX\
 =========xxxxxx=======================\
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  row: 21,
+  col: 38
 
+ }
+
+ mapArr[1] = map1;
+ mapArr[2] = map2;
+ mapArr[3] = map3;
+
+
+
+function init(mapNo) {
+
+	canvas.height = Math.min(mapNo.row * 32, 500);
+    canvas.width = Math.min(mapNo.col * 16, 480);
+
+	context.imageSmoothingEnabled = false;
+	player = new Player();
+	map = new Map(tilesheet, 16, 32);
+	map.init(mapNo.col, mapNo.row, mapNo.map);
 
 	cam = new Camera(player, map);
 	loop(); // finish initializing and start the game loop
 
 }
 
-
-
-
-
 function loop() {
-
 
 	// clearRect functions below but if u redraw the bg every time it's not necessary
 	//context.clearRect(player.camCoords.x,player.camCoords.y,64,64); 
@@ -111,31 +163,19 @@ function loop() {
 	context.fillStyle = '#e1ecf2';
 	context.fillRect(0,0,canvas.width,canvas.height);
 	
-	//drawMap();
 	player.update();
-	/*
-	// temp floor collision
-	if(player.y >= 220) {
-		player.y = 220;
-		player.y_vel = 0;
-		player.jumping = false;
-	};*/
 
 	collisionHandler(player, map);
 
+    
 	cam.update();
 	coinRemove(player);
 	spike(player);
 	cam.draw();
 	player.draw();
-
-	//console.log(player.y_vel);
 	
     window.requestAnimationFrame(loop);
 }
-
-
-
 
 window.addEventListener("keydown", control.keyListener);
 window.addEventListener("keyup", control.keyListener);
