456a3a4bde3e39bd5470931a611b3bae22c49181
bananery <40243071+bananery@users.noreply.github.com>
2018-06-25 14:58:18 -0400

    Add files via upload

---
+++ vbuggy/maps.js
@@ -0,0 +1,115 @@
+
+/**********
+
+this file contains maps
+
+**********/
+
+var mapArr = [];
+
+
+mapArr.push({
+	map: "\
+ ]                                 [X \
+ ]                                 [X \
+ ]                      t          [X \
+ ]                     dmb         [X \
+ ]                                 [X \
+ ]              db           %%    [X \
+ ]                            %    [X \
+ ]           %     dmmb     o      [X \
+ ]         o              dmmmmb   [X \
+ ]       r====7                    [X \
+ ]   o   ixxxxl  !    %        !   [X \
+ ========xxxxxx====================== \
+ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ",
+   row: 13, 
+   col: 38
+});
+
+mapArr.push({
+    map: "\
+]                        o        [X\
+]                                 [X\
+]            o    o               [X\
+]                     vt   o      [X\
+]        o          dmmmb         [X\
+]    o          o         o       [X\
+]       o   dmmmb   o     o   o   [X\
+]                  o     o        [X\
+] o  dmmmb  o    o    o  o        [X\
+]            vvv     o   o      o [X\
+]    o   o  dmmmb        vv  o    [X\
+]                   o  dmmmb      [X\
+]v o        o  o            o     [X\
+X]      dmmmb      dmmmmb         [X\
+XXX]              ! o       o     [X\
+XX]    o    %     dmmb o  o !o    [X\
+]o        %  !  o    o   dmmmmb   [X\
+]! oooo dmmmmb o o      o  o  o   [X\
+]2   o  ixxxxl   ovvvvo !    !  o [X\
+========xxxxxx======================\
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  row: 21,
+  col: 36
+
+ });
+
+mapArr.push({
+    map: "\
+X]                        o        [XX\
+X]                                 [XX\
+X]  dmb          o    o            [XX\
+X8445                     v    o   [XX\
+XXXX]        o          dmb        [XX\
+XXXX]    o          o         o    [XX\
+X9776       o   dmmmb   o     o   o[XX\
+X]       2          o     o    dmb [XX\
+X] o  dmb  o    o    o  o          [XX\
+X]            vvv     o   o      o [XX\
+X]    o   o  dmb        vv  o      [XX\
+X]                   o  dmb        [XX\
+X]v o        o  o  2         o     [XX\
+X8445    dmmmb      dmb            [XX\
+XXXX]              ! o       o     [XX\
+X9776   o    %     dmmb o  t !o    [XX\
+X]o        %  !  o    o   dmmmmb   [XX\
+X]! dmmmmmmmmmb o o      o  o  o   [XX\
+X]!!!!!!!ixxxxl!!!!!!!!!!!!!!!!!!!![XX\
+=========xxxxxx=======================\
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  row: 21,
+  col: 38
+
+ });
+
+
+mapArr.push({
+    map: "\
+X]                        o        [XX\
+X]                                 [XX\
+X]  dmb          o    o            [XX\
+X8445                     v    o   [XX\
+XXXX]        o          dmb        [XX\
+XXXX]    o          o         o    [XX\
+X9776       o   dmmmb   o     o   o[XX\
+X]       2          o     o    dmb [XX\
+X] o  dmb  o    o    o  o          [XX\
+X]            vvv     o   o      o [XX\
+X]    o   o  dmb        vv  o      [XX\
+X]                   o  dmb        [XX\
+X]v o        o  o  2         o     [XX\
+X8445    dmmmb      dmb            [XX\
+XXXX]              ! o       o     [XX\
+X9776   o    %     dmmb o  o !o    [XX\
+X]o        %  !  o    o   dmmmmb   [XX\
+X]! dmmmmmmmmmb o o      o  o  o   [XX\
+X]!!!!!!!ixxxxl!!!!!!!!!!!!!!!!!!!![XX\
+=========xxxxxx=======================\
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  row: 21,
+  col: 38
+
+ });
+
+
