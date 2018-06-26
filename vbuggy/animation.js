b8be1b14723c440346e78f33aa5c1d8b13aae280
bananery <40243071+bananery@users.noreply.github.com>
2018-06-26 15:26:30 -0400

    Add files via upload

---
+++ vbuggy/animation.js
@@ -0,0 +1,141 @@
+/**********
+
+this file contains vars/methods for animation.
+
+**********/
+
+Player.prototype.animation = {
+		state: "idle",
+		delay: 10,
+		cur_frame: 0,
+		max_frames: 64, // just so cur_frame doesn't get huge i gues
+
+		idle: {},
+
+		walk_right: {
+			no_frames: 4,
+			frames: [{x:32,y:0}, {x:64,y:0}, {x:0,y:32}, {x:64,y:0}]
+		},
+
+		walk_left: {
+			no_frames: 4,
+			frames: [{x:32,y:304}, {x:64,y:304}, {x:64,y:336}, {x:64,y:304} ]
+		},
+
+		jump_right: {
+			no_frames: 1,
+			frames: [{x:32,y:32}]
+		},
+
+		jump_left: {
+			no_frames: 1,
+			frames: [{x:32,y:336}]
+		},
+
+		land_right: {
+			no_frames: 1,
+			frames: [{x:64,y:32}]
+		},
+
+		land_left: {
+			no_frames: 1,
+			frames: [{x:0,y:336}]
+		},
+	};
+
+Player.prototype.setAnimationFrame = function() {
+
+	switch(this.animation.state) {
+
+		case "jump_right":
+			this.frame.x = this.animation.jump_right.frames[(this.animation.cur_frame % this.animation.jump_right.no_frames)].x;
+			this.frame.y = this.animation.jump_right.frames[(this.animation.cur_frame % this.animation.jump_right.no_frames)].y;
+			break;
+		case "land_right":
+			this.frame.x = this.animation.land_right.frames[(this.animation.cur_frame % this.animation.land_right.no_frames)].x;
+			this.frame.y = this.animation.land_right.frames[(this.animation.cur_frame % this.animation.land_right.no_frames)].y;
+			break;
+		case "walk_right":
+			this.frame.x = this.animation.walk_right.frames[(this.animation.cur_frame % this.animation.walk_right.no_frames)].x;
+			this.frame.y = this.animation.walk_right.frames[(this.animation.cur_frame % this.animation.walk_right.no_frames)].y;
+			break;
+		case "idle_right":
+			this.frame.x = 0;
+			this.frame.y = 0;
+			break;
+
+		case "jump_left":
+			this.frame.x = this.animation.jump_left.frames[(this.animation.cur_frame % this.animation.jump_left.no_frames)].x;
+			this.frame.y = this.animation.jump_left.frames[(this.animation.cur_frame % this.animation.jump_left.no_frames)].y;
+			break;
+		case "land_left":
+			this.frame.x = this.animation.land_left.frames[(this.animation.cur_frame % this.animation.land_left.no_frames)].x;
+			this.frame.y = this.animation.land_left.frames[(this.animation.cur_frame % this.animation.land_left.no_frames)].y;
+			break;
+		case "walk_left":
+			this.frame.x = this.animation.walk_left.frames[(this.animation.cur_frame % this.animation.walk_left.no_frames)].x;
+			this.frame.y = this.animation.walk_left.frames[(this.animation.cur_frame % this.animation.walk_left.no_frames)].y;
+			break;
+		case "idle_left":
+			this.frame.x = 0;
+			this.frame.y = 304;
+			break;
+
+	}
+}
+
+
+Player.prototype.updateAnimation = function() {
+
+	// frame delay for all player animations .. idk theres probably a more elegant way to do this
+	if (time%this.animation.delay === 0 ) this.animation.cur_frame = (this.animation.cur_frame+1) % this.animation.max_frames;
+
+
+	if (this.facingRight) {
+		if (this.y_vel < -1) this.animation.state = "jump_right";
+		else if (this.y_vel > 1) this.animation.state = "land_right";
+		else if (this.x_vel > 1) this.animation.state = "walk_right";
+		else this.animation.state = "idle_right";
+
+	}
+
+	else {
+		if (this.y_vel < -1) this.animation.state = "jump_left";
+		else if (this.y_vel > 1) this.animation.state = "land_left";
+		else if (this.x_vel < -1) this.animation.state = "walk_left";
+		else this.animation.state = "idle_left";
+	}
+
+	this.setAnimationFrame();
+
+};
+
+
+var coinAnimation = {
+	cur_frame: 0,
+	no_frames: 4,
+	frames: [{x:64,y:112}, {x:80,y:112}, {x:96,y:112}, {x:112,y:112}],
+	lastUpdate: 0,
+	delay: 15
+};
+
+coinAnimation.getFrame = function() {
+	if (time%this.delay === 0 && time != this.lastUpdate) {
+		this.cur_frame = (this.cur_frame+1) % this.no_frames; // update current frame after delay
+		this.lastUpdate = time;
+	}
+	return this.frames[this.cur_frame];
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vbuggy/player.js
+++ vbuggy/player.js
@@ -17,7 +17,7 @@ function Player() {
 
 	this.camCoords = {}; 
 	
-    	this.GRAVITY = 0.15*scaleFactor; //0.15
+    this.GRAVITY = 0.15*scaleFactor; //0.15
 	this.X_ACCEL = 0.25*scaleFactor; // 0.25
 	this.Y_ACCEL = 9*scaleFactor;
 	this.FRICTION = 0.83;
@@ -42,43 +42,6 @@ function Player() {
 	};
 	this.facingRight = true;
 
-	this.animation = {
-		state: "idle",
-		cur_frame: 0,
-		max_frames: 64, // just so cur_frame doesn't get huge i gues
-
-		idle: {},
-
-		walk_right: {
-			no_frames: 4,
-			frames: [{x:32,y:0}, {x:64,y:0}, {x:0,y:32}, {x:64,y:0}]
-		},
-
-		walk_left: {
-			no_frames: 4,
-			frames: [{x:32,y:304}, {x:64,y:304}, {x:64,y:336}, {x:64,y:304} ]
-		},
-
-		jump_right: {
-			no_frames: 1,
-			frames: [{x:32,y:32}]
-		},
-
-		jump_left: {
-			no_frames: 1,
-			frames: [{x:32,y:336}]
-		},
-
-		land_right: {
-			no_frames: 1,
-			frames: [{x:64,y:32}]
-		},
-
-		land_left: {
-			no_frames: 1,
-			frames: [{x:0,y:336}]
-		},
-	};
 }
 
 
@@ -142,74 +105,8 @@ Player.prototype.draw = function() {
 };
 
 
-Player.prototype.setAnimationFrame = function() {
+// **** animation functions & frame information are in animation.js
 
-	switch(this.animation.state) {
-
-		case "jump_right":
-			this.frame.x = this.animation.jump_right.frames[(this.animation.cur_frame % this.animation.jump_right.no_frames)].x;
-			this.frame.y = this.animation.jump_right.frames[(this.animation.cur_frame % this.animation.jump_right.no_frames)].y;
-			break;
-		case "land_right":
-			this.frame.x = this.animation.land_right.frames[(this.animation.cur_frame % this.animation.land_right.no_frames)].x;
-			this.frame.y = this.animation.land_right.frames[(this.animation.cur_frame % this.animation.land_right.no_frames)].y;
-			break;
-		case "walk_right":
-			this.frame.x = this.animation.walk_right.frames[(this.animation.cur_frame % this.animation.walk_right.no_frames)].x;
-			this.frame.y = this.animation.walk_right.frames[(this.animation.cur_frame % this.animation.walk_right.no_frames)].y;
-			break;
-		case "idle_right":
-			this.frame.x = 0;
-			this.frame.y = 0;
-			break;
-
-		case "jump_left":
-			this.frame.x = this.animation.jump_left.frames[(this.animation.cur_frame % this.animation.jump_left.no_frames)].x;
-			this.frame.y = this.animation.jump_left.frames[(this.animation.cur_frame % this.animation.jump_left.no_frames)].y;
-			break;
-		case "land_left":
-			this.frame.x = this.animation.land_left.frames[(this.animation.cur_frame % this.animation.land_left.no_frames)].x;
-			this.frame.y = this.animation.land_left.frames[(this.animation.cur_frame % this.animation.land_left.no_frames)].y;
-			break;
-		case "walk_left":
-			this.frame.x = this.animation.walk_left.frames[(this.animation.cur_frame % this.animation.walk_left.no_frames)].x;
-			this.frame.y = this.animation.walk_left.frames[(this.animation.cur_frame % this.animation.walk_left.no_frames)].y;
-			break;
-		case "idle_left":
-			this.frame.x = 0;
-			this.frame.y = 304;
-			break;
-
-	}
-
-}
-
-
-Player.prototype.updateAnimation = function() {
-
-	// frame delay for all player animations .. idk theres probably a more elegant way to do this
-	if (time%10 === 0 ) this.animation.cur_frame = (this.animation.cur_frame+1) % this.animation.max_frames;
-
-
-	if (this.facingRight) {
-		if (this.y_vel < -1) this.animation.state = "jump_right";
-		else if (this.y_vel > 1) this.animation.state = "land_right";
-		else if (this.x_vel > 1) this.animation.state = "walk_right";
-		else this.animation.state = "idle_right";
-
-	}
-
-	else {
-		if (this.y_vel < -1) this.animation.state = "jump_left";
-		else if (this.y_vel > 1) this.animation.state = "land_left";
-		else if (this.x_vel < -1) this.animation.state = "walk_left";
-		else this.animation.state = "idle_left";
-	}
-
-	this.setAnimationFrame();
-
-
-};
 
 
 
