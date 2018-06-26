e83da7a3964d6e668e8d77efc49e695a04bb009b
zexiin <35815821+zexiin@users.noreply.github.com>
2018-06-26 11:12:12 -0400

    Update enemy.js

--- vbuggy/enemy.js
+++ vbuggy/enemy.js
@@ -2,12 +2,61 @@
 
 // needs acceleration, x, y 
 
-function Enemy(x, y, a) {
+// collection of enemies
+function Enemies(arrayRep, cam) {
+
+	this.enemyBag = [];
+	//this.cam = cam;
+
+	let enemyIndex = getAllIndexes(arrayRep, "Q");
+
+	for (let i = 0; i < enemyIndex.length; i++) {
+
+		let y = Math.floor(enemyIndex[i] / map.cols);
+		let x = Math.floor(enemyIndex[i] % map.cols);
+
+		this.enemyBag.push(new Enemy(x, y, 0, 4));
+	}
+
+}
+
+Enemies.prototype.update = function() {
+
+	this.enemyBag.forEach(function(element) {
+    element.update();
+   });
+}
+
+Enemies.prototype.draw = function() {
+
+	this.enemyBag.forEach(function(element) {
+    element.draw(cam);
+   
+   });
+}
+
+// helper method
+function getAllIndexes(arr, val) {
+
+    var indexes = [], i = -1;
+
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i);
+    }
+
+    return indexes;
+}
+
+// single enemy
+function Enemy(x, y, a, l) {
 	
 		this.x = x;
 		this.y = y;
-		this.x_vel = 0.4;
+		this.x_vel = 0.02;
 		this.X_ACCEL = a;
+        
+		this.length = l; // length of path enemy is to travel
+		this.currDist = 0; // distance he has travelled on current path (if it reaches l it turns around)
 }
 
 // with each update: update x-vel of enemy
@@ -18,6 +67,12 @@ Enemy.prototype.update = function() {
 	this.x_vel += this.X_ACCEL;
 
 	this.x += this.x_vel;
+
+	this.currDist += this.x_vel;
+
+	if (Math.abs(this.currDist) > this.length) { 
+		this.turn();
+	}
 
 	this.checkKill();
 
@@ -45,26 +100,29 @@ Enemy.prototype.checkKill = function() {
 		player.reset();
 		
 	}
-
-
-
 	
 }
 
-Enemy.prototype.die = function() {
+Enemy.prototype.turn = function() {
 
-
+   this.x_vel *= -1;
+   this.currDist = 0;
 
 }
 
-Enemy.prototype.draw = function(cam) {
+
+Enemy.prototype.draw = function() {
 
 	// if his x & y coordinates are within the x y coordiantes of the camera, draw:
 	// draw him at the camera coordinates
+	let xx = this.x * scaleFactor * 16;
+	let yy = this.y * scaleFactor * 16;
 
-	if (this.x >= cam.x && this.x <= (cam.x + cam.w) && this.y >= cam.y && this.y <= (cam.y + cam.h)) {
+	if (xx >= cam.x && xx <= (cam.x + cam.w) && yy >= cam.y && yy 
 
-		let xyTarget = cam.mapToCam(this.x, this.y);
+		<= (cam.y + cam.h)) {
+
+	let xyTarget = cam.mapToCam(this.x * 16 * scaleFactor, this.y * 16 * scaleFactor);
 
 		context.drawImage(
 				map.tilesheet, // image src
