5c097b3f93e3763401520e743c427c9c919e28d0
zexiin <35815821+zexiin@users.noreply.github.com>
2018-06-27 10:32:12 -0400

    Update maps.js

--- vbuggy/maps.js
+++ vbuggy/maps.js
@@ -7,25 +7,24 @@ this file contains maps
 
 var mapArr = [];
 
-
 mapArr.push({
 	level: 1,
 	map: "\
-]                                 [X \
-]                                 [X \
-]                      t          [X \
-]                     dmb         [X \
-]                                 [X \
-]              db           %%    [X \
-]                 Q          %    [X \
-]           %     dmmb  Q  o      [X \
-]       Q o             dmmmmb    [X \
-]       r====7                    [X \
-]   o   ixxxxl  !    %        !   [X \
-========xxxxxx====================== \
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ",
+]                                 [X\
+]                                 [X\
+]                      t          [X\
+]                     dmb         [X\
+]                                 [X\
+]              db           %%    [X\
+]                 Q          %    [X\
+]           %     dmmb  Q  o      [X\
+]       Q o             dmmmmb    [X\
+]       r====7                    [X\
+]   o   ixxxxl  !    %    P   !   [X\
+========xxxxxx======================\
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
    row: 13, 
-   col: 37
+   col: 36
 });
 
 mapArr.push({
@@ -40,15 +39,15 @@ mapArr.push({
 ]       o   dmmmb   o     o   o   [X\
 ]    Q             o     o        [X\
 ] o  dmmmb  o    o    o  o        [X\
-]           Q        o   o      o [X\
+]            vvv     o   o      o [X\
 ]    o   o  dmmmb      Q vv  o    [X\
 ]                   o  dmmmb      [X\
-]v o    Q   o  o    Q       o     [X\
+]v o        o  o    Q       o     [X\
 ]       dmmmb      dmmmmb         [X\
-]                 Q o       o     [X\
+]                 ! o       o     [X\
 ]    o      %     dmmb o  o !o    [X\
-]o      Q %  !  o    o   dmmmmb   [X\
-]! oooo dmmmmb o Q      o  o  o   [X\
+]o        %  !  o    o   dmmmmb   [X\
+]! oooo dmmmmb o o   P  o  o  o   [X\
 ]2   o  ixxxxl   ovvvvo !    !  o [X\
 ========xxxxxx======================\
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
@@ -74,9 +73,9 @@ mapArr.push({
 %                   o  dmb        %\
 %  o    Q   o  o  2         o     %\
 %       dmmmb      dmb            %\
-%              Q  ! o       o     %\
+%              P  ! o       o     %\
 %   o    %     dmmb o  oQ!o       %\
-%        %  !  o        dmmmmb    %\
+%        % P!  o        dmmmmb    %\
 %  !    r====7      db            %\
 %!!!!!!!ixxxxl!!!!!!!!!!!!!!!!!!!!%\
 =========xxxxxx====================\
@@ -90,29 +89,29 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
 mapArr.push({
 	level: 4,
     	map: "\
-X]                        o        [XX\
-X]                                 [XX\
-X]  dmb          o    o            [XX\
-X]                        v    o   [XX\
-X]        o          dmb           [XX\
-X]    o         Qo         o       [XX\
-X]          o   dmmmb   o     o   o[XX\
-X]       2          o     o    dmb [XX\
-X] o  dmb  o    o    o  o          [XX\
-X]            vvv     o   o      o [XX\
-X]    o   o  dmb        vv  o      [XX\
-X]                   o  dmb        [XX\
-X]v o    Q   o  o  2         o     [XX\
-X]       dmmmb      dmb            [XX\
-X]              ! oQ      o        [XX\
-X]      o    %     dmmb o Qo !o    [XX\
-X]o  Q     %  !  o    o   dmmmmb   [XX\
-X]! dmmmmmmmmmb o o      o  o  o   [XX\
-X]!!!!!!!ixxxxl!!!!!!!!!!!!!!!!!!!![XX\
-=========xxxxxx=======================\
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+]                        o        [\
+]                                 [\
+]  dmb          o    o            [\
+]                        v    o   [\
+]        o          dmb           [\
+]    o      Q  Qo         o       [\
+]          o   dmmmb   o     o   o[\
+]    P  2          o     o    dmb [\
+] o  dmb  o    o    o  o          [\
+]            vvv     o   o      o [\
+]    o   o  dmb        vv  o      [\
+]                   o  dmb        [\
+]v o    Q   o  o  2         o     [\
+]       dmmmb      dmb            [\
+]              ! oQ      o        [\
+]      o    %     dmmb o QoP!o    [\
+]o  Q     %  !  o    o   dmmmmb   [\
+]! dmmmmmmmmmb o o      o  o  o   [\
+]!!!!!!!ixxxxl!!!!!!!!!!!!!!!!!!!![\
+========xxxxxx=====================\
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   row: 21,
-  col: 38
+  col: 35
 
  });
 
