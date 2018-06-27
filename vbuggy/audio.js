287f0fc0785d8198db7db946650240f7e2d1694e
Kittytoee <40214897+Kittytoee@users.noreply.github.com>
2018-06-27 11:50:03 -0400

    Add files via upload

---
+++ vbuggy/audio.js
@@ -0,0 +1,21 @@
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    };
+    this.stop = function(){
+        this.sound.pause();
+    };
+}
+
+
+function startaudio() {
+    mySound = new sound("bounce.mp3");
+    myMusic = new sound("gametheme.mp3");
+    myMusic.play();
+}
