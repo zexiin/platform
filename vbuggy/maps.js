318e0e49844c660a2c90f980e24ec588134cdb9c
zexiin <35815821+zexiin@users.noreply.github.com>
2018-06-27 11:10:47 -0400

    Update maps.js

--- vbuggy/maps.js
+++ vbuggy/maps.js
@@ -14,13 +14,13 @@ mapArr.push({
 ]                                 [X\
 ]                      t          [X\
 ]                     dmb         [X\
-]                                 [X\
+]                    P            [X\
 ]              db           %%    [X\
-]                 Q          %    [X\
-]           %     dmmb  Q  o      [X\
-]       Q o             dmmmmb    [X\
-]       r====7                    [X\
-]   o   ixxxxl  !    %    P   !   [X\
+]                 3          %    [X\
+]           %     dmmb  5  o      [X\
+]       5 o             dmmmmb    [X\
+]       r====k                    [X\
+]   o   ixxxxl  !    %        !   [X\
 ========xxxxxx======================\
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
    row: 13, 
@@ -35,14 +35,14 @@ mapArr.push({
 ]            o    o               [X\
 ]                     vt   o      [X\
 ]        o          dmmmb         [X\
-]    o      Q   o         o       [X\
+]    o      4   o         o       [X\
 ]       o   dmmmb   o     o   o   [X\
-]    Q             o     o        [X\
+]    4             o     o        [X\
 ] o  dmmmb  o    o    o  o        [X\
 ]            vvv     o   o      o [X\
-]    o   o  dmmmb      Q vv  o    [X\
+]    o   o  dmmmb      4 vv  o    [X\
 ]                   o  dmmmb      [X\
-]v o        o  o    Q       o     [X\
+]v o        o  o   5        o     [X\
 ]       dmmmb      dmmmmb         [X\
 ]                 ! o       o     [X\
 ]    o      %     dmmb o  o !o    [X\
@@ -64,19 +64,19 @@ mapArr.push({
 %  dmb          o    o            %\
 %                        v    o   %\
 %           o          dmb        %\
-%       o      Q   o         o    %\
+%       o      4   o         o    %\
 %          o   dmmmb   o     o t o%\
-%       2          o     o    dmb %\
+%    2             o     o    dmb %\
 % o  dmb  o    o    o  o          %\
 %            vvv     o   o      o %\
 %    o   o  dmb        vv  o      %\
 %                   o  dmb        %\
-%  o    Q   o  o  2         o     %\
+%  o    4   o  o   2        o     %\
 %       dmmmb      dmb            %\
 %              P  ! o       o     %\
-%   o    %     dmmb o  oQ!o       %\
+%   o    %     dmmb o  o5!o       %\
 %        % P!  o        dmmmmb    %\
-%  !    r====7      db            %\
+%  !    r====k      db            %\
 %!!!!!!!ixxxxl!!!!!!!!!!!!!!!!!!!!%\
 =========xxxxxx====================\
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
@@ -94,18 +94,18 @@ mapArr.push({
 ]  dmb          o    o            [\
 ]                        v    o   [\
 ]        o          dmb           [\
-]    o      Q  Qo         o       [\
+]    o         5o         o       [\
 ]          o   dmmmb   o     o   o[\
-]    P  2          o     o    dmb [\
+]    P             o     o    dmb [\
 ] o  dmb  o    o    o  o          [\
 ]            vvv     o   o      o [\
 ]    o   o  dmb        vv  o      [\
 ]                   o  dmb        [\
-]v o    Q   o  o  2         o     [\
+]v o    4   o  o            o     [\
 ]       dmmmb      dmb            [\
-]              ! oQ      o        [\
-]      o    %     dmmb o QoP!o    [\
-]o  Q     %  !  o    o   dmmmmb   [\
+]              ! o3      o        [\
+]      o    %     dmmb o 5oP!o    [\
+]o 9     %  !  o    o   dmmmmb    [\
 ]! dmmmmmmmmmb o o      o  o  o   [\
 ]!!!!!!!ixxxxl!!!!!!!!!!!!!!!!!!!![\
 ========xxxxxx=====================\
