880d0a9ad426110aa438ebb65ddc6f076a1921ab
zexiin <35815821+zexiin@users.noreply.github.com>
2018-07-09 16:20:46 -0400

    Add files via upload

---
+++ fresh-baby/audio.js
@@ -0,0 +1,14 @@
+var win = new Audio("../assets/audio/win.mp3");
+var die = new Audio("../assets/audio/negative_long.mp3");
+var coin_sound = new Audio("../assets/audio/coin.mp3");
+var water_enter = new Audio("../assets/audio/water_drip.mp3");
+var water_exit = new Audio("../assets/audio/water_exit.mp3");
+
+var bounce = new Audio("../assets/audio/bounce.mp3");
+var footstep = new Audio("../assets/audio/footstep_short.wav");
+var underwater = new Audio("../assets/audio/underwater.mp3");
+
+var icecrack = new Audio("../assets/audio/icecrack.mp3");
+
+
+
