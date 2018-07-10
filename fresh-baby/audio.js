167f29e8dd4191d8f87715c217d2947b5a2b3db2
zexiin <35815821+zexiin@users.noreply.github.com>
2018-07-10 11:58:53 -0400

    Update audio.js

--- fresh-baby/audio.js
+++ fresh-baby/audio.js
@@ -9,7 +9,7 @@ var footstep = new Audio("../assets/audio/footstep_short.wav");
 var underwater = new Audio("../assets/audio/underwater.mp3");
 
 var icecrack = new Audio("../assets/audio/icecrack4.mp3");
-var bg_music = new Audio("../assets/audio/bg3.mp3");
+var bg_music = new Audio("../assets/audio/bground.mp3");
 
 
 
