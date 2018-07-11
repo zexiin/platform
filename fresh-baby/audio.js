4e210d72074809ccf066b7739dd2d347bb200409
zexiin <35815821+zexiin@users.noreply.github.com>
2018-07-11 11:27:47 -0400

    Update audio.js

--- fresh-baby/audio.js
+++ fresh-baby/audio.js
@@ -1,3 +1,17 @@
+var sound = new Bag();
+
+sound.bag.push(new Audio("../assets/audio/win.mp3")); // 0
+sound.bag.push(new Audio("../assets/audio/negative_long.mp3")); // 1 DYING
+sound.bag.push(new Audio("../assets/audio/coin.mp3")); // 2
+sound.bag.push(new Audio("../assets/audio/water_drip.mp3")); // 3
+sound.bag.push(new Audio("../assets/audio/water_exit.mp3")); // 4
+
+sound.bag.push(new Audio("../assets/audio/bounce.mp3")); // 5
+sound.bag.push(new Audio("../assets/audio/footstep_short.wav")); // 6 - currently unused
+sound.bag.push(new Audio("../assets/audio/underwater.mp3")); // 7
+sound.bag.push(new Audio("../assets/audio/icecrack4.mp3")); // 8
+sound.bag.push(new Audio("../assets/audio/01 Frozen Langos.mp3")); // 9
+
 var win = new Audio("../assets/audio/win.mp3");
 var die = new Audio("../assets/audio/negative_long.mp3");
 var coin_sound = new Audio("../assets/audio/coin.mp3");
