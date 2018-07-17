da5d8f726dee11ba4c7be683fd988d4948ebcecc
bananery <40243071+bananery@users.noreply.github.com>
2018-07-17 16:28:01 -0400

    Update map_classes.js

--- fresh-baby/map_classes.js
+++ fresh-baby/map_classes.js
@@ -84,25 +84,42 @@ class TileMap extends Map {
 	    	case "[": return 0;
 	    	case "X": return 0;
 
-	    	case "%": return 155; // solid
-	    	case "O": return 135; // green brick
+	    	case "%": if(winter_on) return 135; else return 155; // solid
+	    	case "+": if(winter_on) return 83; else return 87; // bridge
 	    	
-	    	case "d": return 80;
-	    	case "x": return 125;
 	    	
-	    	case "m": return 81;
-	    	case "b": return 82;
-	    	case "i": return 124;
-	    	case "l": return 126;
-	    	case "=": return 103;
-	    	case "r": return 102;
-	    	case "k": return 104;
-	    	case "-": return 213; // dirt top
-	    	case "y": return 212; // dirt topleft corner
-	    	case "j": return 214; // dirt topright
-	    	case "L": return 146; // dirt bottom left
-	    	case "K": return 148; //dirt bottom right
-	    	case "_": return 147;// dirt bottom
+	    	case "x": if(winter_on) return 129; else return 125;
+
+	    	case "d": if(winter_on) return 84; else return 80; // floating platforms
+	    	case "m": if(winter_on) return 85; else return 81;
+	    	case "b": if(winter_on) return 86; else return 82;
+
+	    	case "r": if(winter_on) return 106; else return 102; // top of ground
+	    	case "=": if(winter_on) return 107; else return 103;
+	    	case "k": if(winter_on) return 108; else return 104;
+
+	    	case "i": if(winter_on) return 128; else return 124; // middle dirt layer
+	    	case "x": if(winter_on) return 129; else return 125;
+	    	case "l": if(winter_on) return 130; else return 126;
+	    	
+	    	
+	    	case "y": if(winter_on) return 216; else return 212; // dirt topleft corner
+	    	case "-": if(winter_on) return 217; else return 213; // dirt top
+	    	case "j": if(winter_on) return 218; else return 214; // dirt topright
+
+	    	case "L": if(winter_on) return 150; else return 146; // dirt bottom left
+	    	case "_": if(winter_on) return 151; else return 147;// dirt bottom
+	    	case "K": if(winter_on) return 152; else return 148; //dirt bottom right
+
+
+	    	case "A": if(winter_on) return 172; else return 168; // top of ground (LITE) (TOP COLLISION ONLY)
+	    	case "#": if(winter_on) return 173; else return 169;
+	    	case "E": if(winter_on) return 174; else return 170;
+
+	    	case "a": if(winter_on) return 194; else return 190; // middle dirt layer (LITE) (NO COLLISION)
+	    	case "`": if(winter_on) return 195; else return 191;
+	    	case "e": if(winter_on) return 196; else return 192;
+
 
 	    	case "~": return 220; // water blue 176
 	    	case "I": return 133; // ice
@@ -112,7 +129,7 @@ class TileMap extends Map {
 
 
 	    	case "o": return 159; // coin
-	    	case "t": return 163;
+	    	case "t": return 39; // nxt lv
 	    	case "!": return 232; // spike bloc
 
 	    	case "U": return 35; // superjump powerup
@@ -148,6 +165,11 @@ class CollisionMap extends Map {
 	    	case "=": return 5;  // top
 	    	case "-": return 5;  // top
 	    	case "m": return 5;  // top
+	    	case "+": return 5;  // top
+	    	case "A": return 5;  // top
+	    	case "#": return 5;  // top
+	    	case "E": return 5;  // top
+
 	    	case "d": return 6;  // top & left
 	    	case "y": return 6;  // top & left
 	    	case "r": return 6;  // top & left
@@ -193,40 +215,40 @@ class BGMap extends Map {
 		switch(tile) {
 
 	    	case " ": return 0;
-	    	case "w": return 218; // water, solid bg
+	    	case "w": return 132; // water, solid bg
 
-	    	case "v": return 105; // lil grass sprout
+	    	case "v": if(winter_on) return 109; else return 105; // lil grass sprout or stone
 
 
 	    	// holy shit lol
-	    	case "1": return 14; // tree1
-	    	case "2": return 15; // tree2
-	    	case "3": return 16; // tree3
-	    	case "4": return 36; // tree4
-	    	case "5": return 37; // tree5
-	    	case "6": return 38; // tree6
-	    	case "7": return 58; // tree7
-	    	case "8": return 59; // tree8
-	    	case "9": return 60; // tree9
+	    	case "1": if(winter_on) return 18; else return 14; // tree1
+	    	case "2": if(winter_on) return 19; else return 15; // tree2
+	    	case "3": if(winter_on) return 20; else return 16; // tree3
+	    	case "4": if(winter_on) return 40; else return 36; // tree4
+	    	case "5": if(winter_on) return 41; else return 37; // tree5
+	    	case "6": if(winter_on) return 42; else return 38; // tree6
+	    	case "7": if(winter_on) return 62; else return 58; // tree7
+	    	case "8": if(winter_on) return 63; else return 59; // tree8
+	    	case "9": if(winter_on) return 64; else return 60; // tree9
 
 
 	    	// biggie clouds
-	    	case "a": return 309;
-	    	case ":": return 331; // white
-	    	case "b": return 332;
-	    	case "c": return 333;
-	    	case "d": return 334;
-	    	case "e": return 335;
+	    	case "a": if(winter_on) return 321; else return 309;
+	    	case ":": if(winter_on) return 343; else return 331; // white
+	    	case "b": if(winter_on) return 344; else return 332;
+	    	case "c": if(winter_on) return 345; else return 333;
+	    	case "d": if(winter_on) return 346; else return 334;
+	    	case "e": if(winter_on) return 347; else return 335;
 
-	    	case ".": return 287; // b for bitch (blue)
-	    	case "C": return 290; // single cloud
-	    	case "{": return 265; // halfcloud left
-	    	case "}": return 266; // halfcloud right
+	    	case ".": if(winter_on) return 300; else return 287; // b for bitch (blue)
+	    	case "C": if(winter_on) return 278; else return 290; // single cloud
+	    	case "{": if(winter_on) return 278; else return 265; // halfcloud left
+	    	case "}": if(winter_on) return 279; else return 266; // halfcloud right
 
-	    	case "f": return 353; // grassy hillz
-	    	case "g": return 354;
-	    	case "F": return 375;
-	    	case "G": return 376;
+	    	case "f": if(winter_on) return 366; else return 353; // hillz
+	    	case "g": if(winter_on) return 367; else return 354;
+	    	case "F": if(winter_on) return 388; else return 375;
+	    	case "G": if(winter_on) return 389; else return 376;
 
 
 
@@ -251,22 +273,22 @@ class OverlayMap extends Map {
 
 	    	case " ": return 0;
 
-	    	case "w": return 217; // transparent water, full tile (green)
-	    	case "~": return 216; // transparent water border
+	    	case "w": return 88; // transparent water, full tile
+	    	case "~": return 66; // transparent water border
 
-	    	case "v": return 105; // lil grass sprout
+	    	case "v": if(winter_on) return 109; else return 105; // lil grass sprout or stone
 
 
 	    	// holy shit lol
-	    	case "1": return 14; // tree1
-	    	case "2": return 15; // tree2
-	    	case "3": return 16; // tree3
-	    	case "4": return 36; // tree4
-	    	case "5": return 37; // tree5
-	    	case "6": return 38; // tree6
-	    	case "7": return 58; // tree7
-	    	case "8": return 59; // tree8
-	    	case "9": return 60; // tree9
+	    	case "1": if(winter_on) return 18; else return 14; // tree1
+	    	case "2": if(winter_on) return 19; else return 15; // tree2
+	    	case "3": if(winter_on) return 20; else return 16; // tree3
+	    	case "4": if(winter_on) return 40; else return 36; // tree4
+	    	case "5": if(winter_on) return 41; else return 37; // tree5
+	    	case "6": if(winter_on) return 42; else return 38; // tree6
+	    	case "7": if(winter_on) return 62; else return 58; // tree7
+	    	case "8": if(winter_on) return 63; else return 59; // tree8
+	    	case "9": if(winter_on) return 64; else return 60; // tree9
 
 
 
