03ce6e3bd47d893ae066b15a0edabef9afe6f3db
zexiin <35815821+zexiin@users.noreply.github.com>
2018-07-18 14:37:00 -0400

    Add files via upload

---
+++ fresh-baby/key.js
@@ -0,0 +1,43 @@
+class Key {
+	constructor(x, y) {
+		this.x = this.xinit = x;
+		this.y = this.yinit = y;
+		this.w = this.h = 16 * scaleFactor;
+	}
+
+	reset() {
+	this.x = this.xinit;
+	this.y = this.yinit;
+	player.key = false;
+    };
+
+	update() {
+		console.log(player.key);
+		
+		if (overlap(this, player.bound)) {
+			player.key = true;
+			this.x = this.y = -100;
+		}
+	}
+
+	draw() {
+		if(this.x+this.w >= cam.x && this.x <= cam.x+cam.w 
+			&& this.y+this.h >= cam.y && this.y <= cam.y+cam.h) {
+
+
+			let xyTarget = cam.mapToCam(this.x, this.y);
+
+		        context.drawImage(
+				map.tilesheet, // image src
+				176 * map.tsize % map.tilesheet.width,
+				Math.floor(176 * map.tsize / map.tilesheet.width) * map.tsize,
+				map.tsize, // src width (clipped)
+				map.tsize, // src height (clipped)
+				xyTarget.x, // target x
+				xyTarget.y, // target y
+				map.scaled, map.scaled  // target w,h
+				);
+
+		}
+	}
+}
